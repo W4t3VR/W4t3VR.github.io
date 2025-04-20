793240afa954d7eb8a45066dd6789bb1e354d3fd
SuJin353 <phu6771@gmail.com>
2025-04-20 13:16:53 +0700

    update animation

--- src/js/background.js
+++ src/js/background.js
@@ -1,4 +1,4 @@
-window.onload = function () {
+document.addEventListener("DOMContentLoaded", function () {
 var canvas = document.getElementById('nokey'),
    can_w = parseInt(canvas.getAttribute('width')),
    can_h = parseInt(canvas.getAttribute('height')),
@@ -277,4 +277,4 @@ canvas.addEventListener('mousemove', function(e){
     mouse_ball.y = e.pageY;
     // console.log(mouse_ball);
 });
-}
+});
