ce526c811ed1ecd52bfbb143bf50b1add201ddbd
WatEVR <131243461+W4t3VR@users.noreply.github.com>
2025-08-07 14:33:37 +0700

    test

--- src/js/theme.js
+++ src/js/theme.js
@@ -85,10 +85,10 @@ document.getElementById("carouselImage").src =
 
 // Image is opacity 0 and text is translated off screen by default
 // Add the loaded class to the image and text to animate them in
-window.onload = function () {
+document.addEventListener("DOMContentLoaded", function () {
   document.getElementById("image").classList.add("loaded");
   document.getElementById("text").classList.add("loaded");
   document.getElementsByTagName("html")[0].classList.add("loaded");
   // Preload the remaining images
   preloadImages();
-};
+});
